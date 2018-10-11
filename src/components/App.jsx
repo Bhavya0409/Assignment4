d4342930c765447752448229c453b36969f2d02a
Bhavya Shah <bhavyashah@Bhavyas-MBP.home>
2018-10-11 00:06:01 -0400

    added ability to download md files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,32 +3,65 @@ import ReactMarkdown from 'react-markdown';
 
 class App extends Component {
   state = {
-    markdown: ''
-  }
+    markdown: '',
+    mdName: '',
+    htmlName: ''
+  };
 
   handleChange = (e) => {
-    this.setState({markdown: e.target.value});
-  }
+    this.setState({
+      ...this.state,
+      markdown: e.target.value
+    });
+  };
+
+  updateMdName = (e) => {
+    this.setState({
+      ...this.state,
+      mdName: e.target.value
+    })
+  };
+
+  updateHtmlName = (e) => {
+    this.setState({
+      ...this.state,
+      htmlName: e.target.value
+    })
+  };
+
+  getDisabled = (index) => {
+    const {mdName, htmlName} = this.state;
+    const field = index === 1 ? mdName : htmlName;
+    return field.trim().length === 0 ? 'disabled' : '';
+  };
 
   render() {
-    const {markdown} = this.state;
-    // console.log(markdown);
+    const {markdown, mdName, htmlName} = this.state;
     return (<div className="container">
       <div className="row">
         <div className="col-sm col-md col-lg side left">
-          <div className="header">
-            <h2>Enter Markdown Text</h2>
-            <button className="btn btn-primary">Download</button>
-          </div>
+          <h2>Enter Markdown Text</h2>
           <textarea placeholder="Ex. # Hello Word!" value={markdown} onChange={this.handleChange}/>
-        </div>
-        <div className="col-sm col-md col-lg side right">
-          <div className="header">
-            <h2>Markdown Text Here</h2>
-            <button className="btn btn-primary">Download</button>
+
+          <div className="download-section">
+            <input type="text" placeholder='Name of File (i.e. "my-file")' value={mdName} onChange={this.updateMdName}/>
+            <a className={'btn btn-primary ' + this.getDisabled(1)}
+               download={mdName + '.md'}
+               href={URL.createObjectURL(new Blob([markdown]))}>
+                Download
+            '</a>
           </div>
+        </div>
+
+        <div className="col-sm col-md col-lg side right">
+          <h2>Markdown Text Here</h2>
           <div className="markdown-container">
             <ReactMarkdown source={markdown}/>
+          </div>
+
+          <div className="download-section">
+            <input type="text" placeholder='Name of File (i.e. "my-file")' value={htmlName} onChange={this.updateHtmlName}/>
+              <a className={'btn btn-primary ' + this.getDisabled(2)} download={htmlName + '.html'}>Download</a>
           </div>
         </div>
       </div>
